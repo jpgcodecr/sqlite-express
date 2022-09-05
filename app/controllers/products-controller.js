580597be25a1da86c162606abbdff9710b817068
Jose Pablo Granados <josepablo.granados@groupeconnect.com>
2022-09-05 10:24:41 -0400

    Refactor

--- app/controllers/products-controller.js
+++ app/controllers/products-controller.js
@@ -5,9 +5,7 @@ exports.productsAll = async (req, res) => {
   knex
     .select('*')
     .from('products')
-    .then(userData => {
-      res.json(userData)
-    })
+    .then(userData => res.json(userData))
     .catch(err => {
       res.json({ message: `There was an error retrieving products: ${err}` })
     })
@@ -19,9 +17,7 @@ exports.product = async (req, res) => {
     .select('*')
     .from('products')
     .where({ code: req.params.code })
-    .then(userData => {
-      res.json(userData)
-    })
+    .then(userData => res.json(userData))
     .catch(err => {
       res.json({ message: `There was an error retrieving products: ${err}` })
     })
@@ -36,15 +32,13 @@ exports.productsCreate = async (req, res) => {
       'price': req.body.price,
       'stock': req.body.stock
     })
-    .then(() => {
-      res.json({ message: `Product \'${req.body.name}\' created.` })
-    })
+    .then(() => res.json({ message: `Product \'${req.body.name}\' created.` }))
     .catch(err => {
       res.json({ message: `There was an error creating ${req.body.name} product: ${err}` })
     })
 }
 
-// Edit products
+// Edit product
 exports.productsEdit = async (req, res) => {
 
   const updatedElements = {};
@@ -53,42 +47,33 @@ exports.productsEdit = async (req, res) => {
   if (req.body.price > -1) { updatedElements.price = req.body.price };
   if (req.body.stock > -1) { updatedElements.stock = req.body.stock };
 
-  console.log(req.body);
-  console.log('updatedElements', updatedElements);
-
   knex('products')
     .where({code: req.body.code})
     .update(updatedElements)
-    .then(() => {
-      res.json({ message: `Product ${req.body.code} updated.` })
-    })
+    .then(() => res.json({ message: `Product ${req.body.code} updated.` }))
     .catch(err => {
       res.json({ message: `There was an error updating ${req.body.code} Product: ${err}` })
     })
 }
 
-// Remove products
+// Remove product
 exports.productsDelete = async (req, res) => {
   knex('products')
-    .where('id', req.body.code)
+    .where({ code: req.body.code })
     .del()
-    .then(() => {
-      res.json({ message: `Product ${req.body.code} deleted.` })
-    })
+    .then(() => res.json({ message: `Product ${req.body.code} deleted.` }))
     .catch(err => {
       res.json({ message: `There was an error deleting ${req.body.code} Product: ${err}` })
     })
 }
 
-// Remove all products from database
+// Remove all products
 exports.productsReset = async (req, res) => {
   knex
     .select('*')
     .from('products')
     .truncate()
-    .then(() => {
-      res.json({ message: 'Product list cleared.' })
-    })
+    .then(() => res.json({ message: 'Product list cleared.' }))
     .catch(err => {
       res.json({ message: `There was an error resetting product list: ${err}.` })
     })
